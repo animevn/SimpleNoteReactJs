55681c1391b11b83aea3be195c83ce139f75aa1d
animevn <haanhth21@gmail.com>
2020-03-21 22:15:19 +0700

    flash spinner before populate notes

--- src/Note.js
+++ src/Note.js
@@ -3,15 +3,45 @@ import Card from "./frame/Card";
 import {FirestoreContext} from "./firebase/Firestore";
 
 function Note() {
-  const {notes} = useContext(FirestoreContext);
-
-  return (
-    <div className="row mx-auto">
-      {notes.map((note, index)=>{
-        return <Card key={index} id={note.id} title={note.title} content={note.content} />
-      })}
-    </div>
-  )
+  const {notes, loading} = useContext(FirestoreContext);
+  if (notes.length === 0){
+    if (loading === null){
+      return (
+        <div className="container d-flex justify-content-center">
+          <div className="spinner-grow text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+          <div className="spinner-grow text-secondary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+          <div className="spinner-grow text-success" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+          <div className="spinner-grow text-danger" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+          <div className="spinner-grow text-warning" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+          <div className="spinner-grow text-info" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      )
+    }else {
+      return (
+        <div></div>
+      );
+    }
+  }else {
+    return (
+      <div className="row mx-auto">
+        {notes.map((note, index)=>{
+          return <Card key={index} id={note.id} title={note.title} content={note.content} />
+        })}
+      </div>
+    )
+  }
 }
 
 export default Note;
--- src/firebase/Firestore.js
+++ src/firebase/Firestore.js
@@ -5,19 +5,23 @@ export const FirestoreContext = createContext(null);
 
 export const FirestoreProvider = ({children}) =>{
   const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(null);
 
   useEffect(()=>{
+
     firebase.auth().onAuthStateChanged(user=>{
       const db = firebase.firestore().collection("notes").doc(user.uid).collection("note");
       db.onSnapshot(snapshot => {
+        setLoading(snapshot.size);
         setNotes([]);
         snapshot.forEach(doc=> setNotes(old=>[{...doc.data(), id: doc.id}, ...old]));
       });
     });
+
   }, []);
 
   return (
-    <FirestoreContext.Provider value={{notes}}>
+    <FirestoreContext.Provider value={{notes, loading}}>
       {children}
     </FirestoreContext.Provider>
   );
